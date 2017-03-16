2af3497a674f6fb1e550fd7d7728825e5b41bd55
Paul Allen <pauldallen@gmail.com>
2017-03-16 16:44:31 -0700

    update to variablize the jsrunner-commons version

--- services/config.rb
+++ services/config.rb
@@ -30,7 +30,7 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-cloudwatchlogs" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.8.6"
+                   :version => "${JSRUNNER_COMMONS_VERSION}"
                },
                {
                    :name => "js-yaml",
