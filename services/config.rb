97b8a12d637b342daea55feb53e5e261f394062f
omurbek.jk@gmail.com <omurbek.jk@gmail.com>
2017-08-11 11:16:43 +0600

    CON-185 add s3 notification for json reports

--- services/config.rb
+++ services/config.rb
@@ -39,7 +39,7 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-cloudwatchlogs" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.10.7-beta63"
+                   :version => "1.10.7-beta64"
                },
                {
                    :name => "js-yaml",
@@ -209,5 +209,47 @@ COMPOSITE::coreo_uni_util_jsrunner.tags-rollup-cloudwatchlogs.return
   payload_type 'text'
   endpoint ({
       :to => '${AUDIT_AWS_CLOUDWATCHLOGS_ALERT_RECIPIENT}', :subject => 'CloudCoreo cloudwatchlogs rule results on PLAN::stack_name :: PLAN::name'
+  })
+end
+
+coreo_aws_s3_policy "cloudcoreo-audit-aws-cloudwatchlogs-policy" do
+  action((("${S3_BUCKET_NAME}".length > 0) ) ? :create : :nothing)
+  policy_document <<-EOF
+{
+"Version": "2012-10-17",
+"Statement": [
+{
+"Sid": "",
+"Effect": "Allow",
+"Principal":
+{ "AWS": "*" }
+,
+"Action": "s3:*",
+"Resource": [
+"arn:aws:s3:::${S3_BUCKET_NAME}/*",
+"arn:aws:s3:::${S3_BUCKET_NAME}"
+]
+}
+]
+}
+  EOF
+end
+
+coreo_aws_s3_bucket "cloudcoreo-audit-aws-cloudwatchlogs" do
+  action :create
+  bucket_policies ["cloudcoreo-audit-aws-cloudwatchlogs-policy"]
+  region "us-east-1"
+end
+
+coreo_uni_util_notify "cloudcoreo-audit-aws-cloudwatchlogs-s3" do
+  action((("${S3_BUCKET_NAME}".length > 0) ) ? :notify : :nothing)
+  type 's3'
+  allow_empty true
+  payload 'COMPOSITE::coreo_uni_util_jsrunner.tags-to-notifiers-array-cloudwatchlogs.report'
+  endpoint ({
+      object_name: 'aws-cloudwatchlogs-json',
+      bucket_name: '${S3_BUCKET_NAME}',
+      folder: 'cloudwatchlogs/PLAN::name',
+      properties: {}
   })
 end
